761ad38aa408c1126324d108cbed6c08691550fe
Eric Smith <esmith@8thlight.com>
2017-03-01 09:08:40 -0600

    Setup CPP test runner

---
+++ CoffeeMaker.cpp
@@ -0,0 +1,10 @@
+#include "CoffeeMaker.h"
+
+CoffeeMaker::CoffeeMaker() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CoffeeMaker::~CoffeeMaker() {
+	// TODO Auto-generated destructor stub
+}
---
+++ CoffeeMaker.h
@@ -0,0 +1,12 @@
+#ifndef COFFEEMAKER_H_
+#define COFFEEMAKER_H_
+
+#pragma once
+
+class CoffeeMaker {
+public:
+	CoffeeMaker();
+	virtual ~CoffeeMaker();
+};
+
+#endif
